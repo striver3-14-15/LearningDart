59ea1a3c33de6d686dcd7480554f3037b151394b
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-11-03 18:19:53 +0530

    Learning maps in Dart

---
+++ 03_Collections_in_Dart/03_maps.dart
@@ -0,0 +1,40 @@
+void main() {
+  // Map:
+  /**
+   {
+    key1 : value,
+    key2 : value,
+    key3 : value,
+   }
+   */
+
+  Map marks = {
+    "Rohit": 45,
+    "Shikhar": 97,
+    "Virat": 18,
+  };
+
+  // Accessing the value of Rohit from map
+  print(marks["Rohit"]);
+
+  // Setting the value of Virat to 183
+  marks["Virat"] = 183;
+
+  // Adding new pair of Shreyas with value 96
+  marks["Shreyas"] = 96;
+
+  print(marks);
+
+  // Playing with dynamics of Maps
+  Map<String, int> India = {
+    "Rutu": 19,
+    "Shubhman": 25,
+  };
+}
+
+class Student {
+  final int marks;
+  final String name;
+
+  Student(this.name, this.marks);
+}
