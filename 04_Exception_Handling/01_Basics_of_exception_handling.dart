c4dff2f1b14a927dfc6977d239fc3569ecbf89a0
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-11-04 22:20:50 +0530

    IntegerDivisionByZeroException exception caught

--- 04_Exception_Handling/01_Basics_of_exception_handling.dart
+++ 04_Exception_Handling/01_Basics_of_exception_handling.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: deprecated_member_use
+
+/*
 void main() {
   try {
     print(10 ~/ 0);
@@ -6,5 +9,15 @@ void main() {
     print("Exception caught: $e");
   } finally {
     print("This is finally block and is always executed");
+  }
+}
+*/
+
+void main() {
+  try {
+    int result = 12 ~/ 0;
+    print("The result is $result");
+  } on IntegerDivisionByZeroException {
+    print("Cannot divide by zero");
   }
 }
