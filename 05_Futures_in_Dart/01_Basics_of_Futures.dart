286095eb633b43be572f463c917dc813ab4f7f17
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-11-04 22:55:24 +0530

    example of how to use a 'future' in dart with explanation

--- 05_Futures_in_Dart/01_Basics_of_Futures.dart
+++ 05_Futures_in_Dart/01_Basics_of_Futures.dart
@@ -36,3 +36,42 @@ void main() {
 }
 ```
  */
+
+Future<String> fetchData() {
+  return Future.delayed(Duration(seconds: 5), () {
+    return "Data fetched successfully!";
+  });
+}
+
+void main() {
+  print("Fetching data...");
+  fetchData().then((result) {
+    print(result);
+  });
+  print("Bruh, just wait 5 seconds!!");
+}
+
+
+/*
+- In this example, the fetchData() function returns a Future that simulates fetching data after a delay of 2 seconds.
+
+-  The then() method is used to register a callback function that will be called when the Future is completed.
+
+-  The callback function simply prints the result of the Future to the console.
+
+-  When you run this code, you'll see that "Fetching data..." is printed first, 
+    followed by "Data fetched successfully!" after a delay of 2 seconds.
+*/
+
+
+
+/*
+
+The reason we use Future and not just a delay function is because of the asynchronous nature of certain operations.
+
+In Dart, when you perform a time-consuming operation such as network request, file I/O, or a computation, you don't want to block the rest of your code from executing while waiting for that operation to complete. This is where Future comes in.
+
+A Future allows you to schedule code to run when that operation completes, without blocking the rest of your code. This is done using the `then()` method, which registers a callback function to be called when the Future completes.
+
+If you were to use just a delay function, your entire application would be blocked until the delay is over. This would lead to a poor user experience, as your application would be unresponsive during that time.
+*/
