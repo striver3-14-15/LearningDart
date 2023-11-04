a34a389c93e5dfc93f208f858c146094e9463d87
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-11-04 18:01:04 +0530

    Understanding basics of enum

---
+++ 03_Collections_in_Dart/04_enums.dart
@@ -0,0 +1,53 @@
+
+enum Color {
+  red,
+  green,
+  blue,
+}
+
+enum Status {
+  none,
+  loading,
+  success,
+  error,
+}
+
+void main() {
+  // Example 1: Using enums in a switch statement
+  Color favoriteColor = Color.blue;
+
+  switch (favoriteColor) {
+    case Color.red:
+      print('Your favorite color is red!');
+      break;
+    case Color.green:
+      print('Your favorite color is green!');
+      break;
+    case Color.blue:
+      print('Your favorite color is blue!');
+      break;
+  }
+
+  // Example 2: Using enums as parameters to functions
+  printStatus(Status.none);
+  printStatus(Status.loading);
+  printStatus(Status.success);
+  printStatus(Status.error);
+}
+
+void printStatus(Status status) {
+  switch (status) {
+    case Status.none:
+      print('No status');
+      break;
+    case Status.loading:
+      print('Loading...');
+      break;
+    case Status.success:
+      print('Success!');
+      break;
+    case Status.error:
+      print('Error!');
+      break;
+  }
+}
