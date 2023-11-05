1b5f558960cbb7c4d315e5e991c3aecea7aad27e
Samarth Kulkarni <svkulkarni23@gmail.com>
2023-11-05 11:41:37 +0530

    url using then().catchError()

--- 05_Futures_in_Dart/05_then()_for_web_fetch.dart
+++ 05_Futures_in_Dart/05_then()_for_web_fetch.dart
@@ -4,14 +4,47 @@ import "dart:convert";
 void main() async
 {
   var url = Uri.https("jsonplaceholder.typicode.com",'users');  
+  // var url = Uri.https("jsonplaceholder.typicode.com",'users');  
 
-http.get(url).then(
-  (value) { 
-    print(jsonDecode(value.body)['name']);
-  })
-  .catchError(
-    (err){
-      print(err);
+
+/*
+  var url = Uri.https("jsonplaceholder.typicode.com",'users');  
+  final res = await http.get(url);
+
+  if (res.statusCode == 200) {
+    // Check if the HTTP request was successful
+    List<dynamic> data = jsonDecode(res.body);
+
+    for (var user in data) {
+      print("Name: ${user['name']}");
     }
-  );
+  } else {
+    print("Failed to fetch data: ${res.statusCode}");
+  }
+*/
+// Rather than using the above method, we will use the below one
+  http.get(url).then((res){
+    if(res.statusCode == 200){
+      List data = jsonDecode(res.body);
+      for(var i in data){
+        print("Name : $i");
+      }
+    }
+  }).catchError((e){print("Exception: $e");});
 }
+
+/*
+  http.get(url).then( (res) {
+    if (res.statusCode == 200) {
+      List<dynamic> data = jsonDecode(res.body);
+      for (var user in data) {
+        print("Name: ${user['name']}");
+      }
+    } else {
+      print("Failed to fetch data: ${res.statusCode}");
+    }
+  }).catchError((err) {
+    print(err);
+  });
+
+*/
